7675148be3a22f088cf494ce722b4bccb18d86f7
Matteo-lu <mateolondono.u@gmail.com>
2021-11-09 10:24:34 -0500

    Updating main file with the already inserted verification and the return of the recent_pdf function to a list, style correction in both files

--- sievert_pdf_pipline/main.py
+++ sievert_pdf_pipline/main.py
@@ -5,8 +5,6 @@
 This script allows the user to automate the process to extract especific
 information from a quotation to fill an .xlsx file.
 
-This tool accepts comma separated value files (.xlsx).
-
 This script is required to be located in the same directory as the .pdf
 files as well as the main .xslx
 
@@ -14,64 +12,64 @@ This file contains the main function.
 
 """
 
-import PyPDF2, openpyxl
+import PyPDF2
+import openpyxl
 from pdf_regex_fun import *
 import sys
 
 # Getting the most recent PDF file from the current directory
-recent_pdf_file = recent_pdf()
-if not recent_pdf_file:
-	print("Sorry, inside this directory there are no PDF files")
-	sys.exit(1)
+pdf_list = recent_pdf()
+if len(pdf_list) == 0:
+    print("Sorry, inside this directory there are no PDF files")
+    sys.exit(1)
 
 # opening excel file and setting the worksheet
 try:
-	pipeline_file = openpyxl.load_workbook('Pipeline.xlsx')
-	work_sheet = pipeline_file['Hoja1']
+    pipeline_file = openpyxl.load_workbook('Pipeline.xlsx')
+    work_sheet = pipeline_file['Hoja1']
 except FileNotFoundError:
-	print("Sorry the file Pipeline.xlsx doesn't exist inside this folder")
-	sys.exit(1)
+    print("Sorry the file Pipeline.xlsx doesn't exist inside this folder")
+    sys.exit(1)
 except:
-	print("Unexpected error:", sys.exc_info()[0])
-	sys.exit(1)
+    print("Unexpected error:", sys.exc_info()[0])
+    sys.exit(1)
 
 # Getting the first empty row
 empty_cell = work_sheet.max_row + 1
 
 # Opening the PDF file and saving the text content in pdf_text variable
-try:
-	with open(recent_pdf_file, 'rb') as pdf_file:
-		file_reader = PyPDF2.PdfFileReader(pdf_file)
-		file_page = file_reader.getPage(0)
-		pdf_text = file_page.extractText()
-	if not pdf_text:
-		print("no data in file " + recent_pdf_file)
-except IOError:
-	print("Sorry the file {} doesn't exist inside this folder".format(recent_pdf_file))
-	sys.exit(1)
-except:
-	print("Unexpected error:", sys.exc_info()[0])
-	sys.exit(1)
+for recent_pdf_file in pdf_list:
+    empty_cell = work_sheet.max_row + 1
+    with open(recent_pdf_file, 'rb') as pdf_file:
+        file_reader = PyPDF2.PdfFileReader(pdf_file)
+        file_page = file_reader.getPage(0)
+        pdf_text = file_page.extractText()
+    if not pdf_text:
+        print("no data in file " + recent_pdf_file)
+        continue
 
 # Obtaining the dictionary with the information extracted from pdf
-dict_values = pdf_regex(pdf_text)
-if not dict_values:
-	sys.exit(1)
+    dict_values = pdf_regex(pdf_text)
+    if not dict_values:
+        print("Couldn't find the required content")
+        sys.exit(1)
 
-# Ideinfying if the quotation was already inserted in the .xlsx file
-for row in work_sheet.values:
-	i = 0
-	if (row[0] == dict_values['quot_number']):
-		i += 1
-
-if (i == 0):
-	#Applying style and value to each cell from A# to Q#
-	fill_excel(dict_values, work_sheet, empty_cell)
+    # Ideinfying if the quotation was already inserted in the .xlsx file
+    i = 0
+    for row in work_sheet.values:
+        if (row[0] == dict_values['quot_number']):
+            print("Quote {} already inserted".format(
+                dict_values['quot_number']))
+            i += 1
+    # Applying style and value to each cell from A# to Q#
+    if (i == 0):
+        fill_excel(dict_values, work_sheet, empty_cell)
 
 pipeline_file.save('Pipeline.xlsx')
 pipeline_file.close()
 
-	# Fix style with PEP8 and documentation
-	# Add new functionalities ("Check all files within the directory and instert them if their are not inide the excel file")
-	# Make de proggram portable = "pip install pyinstaller", "got to direcotry -> app", "use pyinstaller --windowed --onefile --icon=./<icon.ico> <app_name>"
-	# Generate windows alert for every error
+# move function to separe files
+# Make de proggram portable = "pip install pyinstaller",
+# "got to direcotry -> app", "use pyinstaller --windowed
+# --onefile --icon=./<icon.ico> <app_name>"
+# Generate windows alert for every error
--- sievert_pdf_pipline/pdf_regex_fun.py
+++ sievert_pdf_pipline/pdf_regex_fun.py
@@ -2,115 +2,125 @@
 
 """spreadsheet auto-fill auxiliar functions"""
 
+
 def pdf_regex(pdf_text):
-	"""Function to extract information from the pdf text variable
-	using regular expressions.
+    """Function to extract information from the pdf text variable
+    using regular expressions.
 
     Args:
         pdf_text: variable containing the text extracted from the pdf file
 
     Returns:
         The return value. dictionary containing the values to be
-		added to the .xlsx file
+        added to the .xlsx file
 
     """
 
-	import re
+    import re
 
-	dict_values = {}
-	# Getting the quotation number
-	quotation_number = re.compile(r'No. *\d\d\d\d')
-	mo = quotation_number.search(pdf_text)
-	if not mo:
-		print("The quote number could not be found")
-		return(None)
-	dict_values['quot_number'] = mo.group()
+    dict_values = {}
+    # Getting the quotation number
+    quotation_number = re.compile(r'No. *\d\d\d\d')
+    mo = quotation_number.search(pdf_text)
+    if not mo:
+        print("The quote number could not be found")
+        return(None)
+    dict_values['quot_number'] = mo.group()
 
-	# Getting the expedition date
-	dates = re.compile(r'(\d\d\W\d\d\W\d\d\d\d)+')
-	mo = dates.search(pdf_text)
-	if not mo:
-		print("The expedition date could not be found")
-		return(None)
-	dates_string = mo.group()
-	dates_list = []
-	dates_list.append(dates_string[:10])
-	dates_list.append(dates_string[10:])
-	dict_values['expedition_date'] = dates_list[0]
+    # Getting the expedition date
+    dates = re.compile(r'(\d\d\W\d\d\W\d\d\d\d)+')
+    mo = dates.search(pdf_text)
+    if not mo:
+        print("The expedition date could not be found")
+        return(None)
+    dates_string = mo.group()
+    dates_list = []
+    dates_list.append(dates_string[:10])
+    dates_list.append(dates_string[10:])
+    dict_values['expedition_date'] = dates_list[0]
 
-	# Responsable
-	dict_values['responsable'] = 'Lina García'
+    # Responsable
+    dict_values['responsable'] = 'Lina García'
 
-	# Getting cliente
-	client_list = re.compile(r'\d{9}\D+')
-	mo = client_list.search(pdf_text)
-	if not mo:
-		print("The client could not be found")
-		return(None)
-	dict_values['client'] = mo.group()[9:]
+    # Getting cliente
+    client_list = re.compile(r'\d{9}\D+')
+    mo = client_list.search(pdf_text)
+    if not mo:
+        print("The client could not be found")
+        return(None)
+    dict_values['client'] = mo.group()[9:]
 
-	# Value before IVA
-	value_list = re.compile(r'Subtotal\$(\d+,)?(\d+,)?(\d+,)?(\d+,)?(\d+.)?(\d+)?')
-	mo = value_list.search(pdf_text)
-	if not mo:
-		print("The Value before IVA could not be found")
-		return(None)
-	dict_values['subtotal'] = mo.group()[9:]
+    # Value before IVA
+    value_list = re.compile(
+        r'Subtotal\$(\d+,)?(\d+,)?(\d+,)?(\d+,)?(\d+.)?(\d+)?'
+    )
+    mo = value_list.search(pdf_text)
+    if not mo:
+        print("The Value before IVA could not be found")
+        return(None)
+    dict_values['subtotal'] = mo.group()[9:]
 
-	return(dict_values)
+    return(dict_values)
+
 
 def fill_excel(dict_values, work_sheet, empty_cell):
-	"""Function to apply style and insert values to each cell from A# to Q#
+    """Function to apply style and insert values to each cell from A# to Q#
 
     Args:
         dict_values: dictionary containing the values to be
-		added to the .xlsx file
-		work_sheet: .xlsx worksheet object
-		empty_cell: first empty cell identify in the .xlsx file
+        added to the .xlsx file
+        work_sheet: .xlsx worksheet object
+        empty_cell: first empty cell identify in the .xlsx file
 
     Returns:
         The return value. None
     """
 
-	from openpyxl.styles import PatternFill, Border, Side, Alignment, Protection, Font
-	from datetime import datetime
+    from openpyxl.styles import PatternFill, Border, Side
+    from openpyxl.styles import Alignment, Protection, Font
+    from datetime import datetime
 
-	# Setting variables fill and align with the expecting cells format
-	fll = PatternFill(start_color="a4c2f4", fill_type = "solid")
-	align = Alignment(horizontal='center', vertical='center', wrapText=True)
+    # Setting variables fill and align with the expecting cells format
+    fll = PatternFill(start_color="a4c2f4", fill_type="solid")
+    align = Alignment(horizontal='center', vertical='center', wrapText=True)
 
-	letter = 'ABCDEFGHIJKLMNOPQ'
-	for cell in letter:
-		if cell == 'A':
-			work_sheet[cell + str(empty_cell)] = dict_values['quot_number']
-		if cell == 'C':
-			date_format = datetime.strptime(dict_values['expedition_date'], '%d/%m/%Y')
-			work_sheet[cell + str(empty_cell)] = date_format.date()
-		if cell == 'F':
-			work_sheet[cell + str(empty_cell)] = dict_values['responsable']
-		if cell == 'H':
-			work_sheet[cell + str(empty_cell)] = dict_values['client']
-		if cell == 'J':
-			work_sheet[cell + str(empty_cell)] = dict_values['subtotal']
+    letter = 'ABCDEFGHIJKLMNOPQ'
+    for cell in letter:
+        if cell == 'A':
+            work_sheet[cell + str(empty_cell)] = dict_values['quot_number']
+        if cell == 'C':
+            date_format = datetime.strptime(
+                dict_values['expedition_date'], '%d/%m/%Y')
+            work_sheet[cell + str(empty_cell)] = date_format.date()
+        if cell == 'F':
+            work_sheet[cell + str(empty_cell)] = dict_values['responsable']
+        if cell == 'H':
+            work_sheet[cell + str(empty_cell)] = dict_values['client']
+        if cell == 'J':
+            work_sheet[cell + str(empty_cell)] = dict_values['subtotal']
 
-		style_cell = work_sheet[cell + str(empty_cell)]
-		style_cell.fill = fll
-		style_cell.alignment = align
+        style_cell = work_sheet[cell + str(empty_cell)]
+        style_cell.fill = fll
+        style_cell.alignment = align
+
 
 def recent_pdf():
-	"""Function to get the most recent PDF file from the current directory
+    """Function to get the most recent PDF file from the current directory
 
     Args:
-		None
+        None
 
     Returns:
         The return value. Most recent file name within the current directory
     """
 
-	import glob, os
+    import glob
+    import os
 
-	file_path = '*.pdf'
-	list_files = sorted(glob.iglob(file_path), key=os.path.getctime, reverse=True)
-	if (len(list_files) == 0):
-		return(None)
-	return (list_files[0])
+    file_path = '*.pdf'
+    list_files = sorted(glob.iglob(file_path),
+                        key=os.path.getctime, reverse=True)
+    if (len(list_files) == 0):
+        return(None)
+    print(list_files)
+    return (list_files)
